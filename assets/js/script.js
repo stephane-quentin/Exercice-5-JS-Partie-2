3986abe8f02be61bb1626a49d3b707e38bf935a0
Stiouf <stephane.quentin@live.fr>
2018-10-09 10:54:47 +0200

    ex5

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+function survol(){
+  document.getElementById('image1').src = 'assets/img/image1_2.jpg';
+  document.getElementById('image2').src = 'assets/img/image2_2.jpg';
+  document.getElementById('image3').src = 'assets/img/image3_2.jpg';
+  document.getElementById('image4').src = 'assets/img/image4_2.jpg';
+  document.getElementById('image5').src = 'assets/img/image5_2.jpg';
+}
